138f913179a0b89a9fe152fb791721f0353d6667
Aljoscha Nyang <a.nyang@gmx.de>
2022-08-26 05:17:13 +0200

    Add basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,25 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              color: Colors.blue,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100,
+                child: Text("Chart"),
+              ),
+              elevation: 5,
+            ),
+            Card(
+              child: Text("List of tx"),
+            ),
+          ],
+        ));
   }
 }
