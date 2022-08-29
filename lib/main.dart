6e9988cc97e770a272050e0501a06ca1e032aa8a
Aljoscha Nyang <a.nyang@gmx.de>
2022-08-29 10:21:45 +0200

    Add transaction class and dummy transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,35 +7,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expenses App',
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "a1",
+      title: "New Shoes",
+      amount: 45.99,
+      purchaseDate: DateTime.now(),
+    ),
+    Transaction(
+      id: "a2",
+      title: "New Shirt",
+      amount: 24.99,
+      purchaseDate: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Card(
-              color: Colors.blue,
-              child: Container(
-                margin: EdgeInsets.all(10),
-                width: double.infinity,
-                height: 100,
-                child: Text("Chart"),
+        body: Container(
+          margin: EdgeInsets.all(5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Card(
+                color: Colors.blue,
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  width: double.infinity,
+                  height: 100,
+                  child: Text("Chart"),
+                ),
+                elevation: 5,
               ),
-              elevation: 5,
-            ),
-            Card(
-              child: Text("List of tx"),
-            ),
-          ],
+              Card(
+                child: Text("List of tx"),
+              ),
+            ],
+          ),
         ));
   }
 }
