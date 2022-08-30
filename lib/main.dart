2ee0e698d2647d9ecb98e55132d12b4845803135
Aljoscha Nyang <a.nyang@gmx.de>
2022-08-30 05:21:10 +0200

    Add user input card

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,32 @@ class MyHomePage extends StatelessWidget {
                 ),
                 elevation: 5,
               ),
+              Card(
+                elevation: 5,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Title",
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Amount",
+                        ),
+                      ),
+                      FlatButton(
+                        textColor: Colors.purple,
+                        onPressed: () {},
+                        child: Text("Add transaction"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
               Column(
                 children: transactions.map((tx) {
                   return Card(
