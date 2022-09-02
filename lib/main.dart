67255012d4550da123897b052ecb5496d8601d0f
Aljoscha Nyang <a.nyang@gmx.de>
2022-09-02 06:14:17 +0200

    Add fonts and use them in app

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Expenses App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: "Quicksand",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: "OpenSans",
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: "OpenSans",
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       home: MyHomePage(),
     );
   }
@@ -72,7 +91,9 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () => _startAddNewTransaction(context),
           ),
         ],
-        title: Text('Flutter App'),
+        title: Text(
+          'Personal Expenses',
+        ),
       ),
       body: SingleChildScrollView(
         child: Container(
