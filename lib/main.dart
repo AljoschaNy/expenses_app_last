0ae90bc3bb71b388d96585ee3bc79fdb7868c1d9
Aljoscha Nyang <a.nyang@gmx.de>
2022-09-12 05:20:06 +0200

    Filter recent transactions from all transactions

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransactions = [];
+  final List<Transaction> _userTransactions = [
+    // Transaction(
+    //   id: "t1",
+    //   title: "New shoes",
+    //   amount: 45.99,
+    //   purchaseDate: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "t2",
+    //   title: "New T-shirt",
+    //   amount: 19.99,
+    //   purchaseDate: DateTime.now(),
+    // ),
+  ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.purchaseDate.isAfter(
+        DateTime.now().subtract(Duration(days: 7)),
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -89,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Chart(_userTransactions),
+              Chart(_recentTransactions),
               TransactionList(_userTransactions),
             ],
           ),
